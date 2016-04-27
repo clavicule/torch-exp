35b475dd525df2297cac2606821abdc7561c05d2
clavicule <claude.cavelius@gmail.com>
2016-04-27 07:32:58 -0500

    adding function split_dataset allows to extract training and testing dataset from full dataset using a ratio

--- helpers/data_io.lua
+++ helpers/data_io.lua
@@ -18,31 +18,29 @@ paths.dofile( 'utils.lua' )
 --   ** image_set.label: 1D tensor containing IDs as integers
 -- [output: classes] an array of string mapping the categories and the table.label IDs
 function load_images_and_labels( folder_path, width, height )
-    classes = {}
+    local classes = {}
     local image_paths = {}
 
     print( 'scanning images' )
     -- for each sub directory
-    for dir in paths.iterdirs(folder_path) do
-        local label = dir
+    for label in paths.iterdirs(folder_path) do
         local subdir = paths.concat(folder_path,label)
 
-        -- for each file
+        -- create the IDs for each label
+        if classes[label] == nil then
+            classes[label] = size_of(classes) + 1
+        end
+
+        -- for each image file
         for img in paths.iterfiles(subdir) do
-
-            -- create the IDs for each label
-            if classes[label] == nil then
-                classes[label] = size_of(classes) + 1
-            end
-
-            local img_fullpath = paths.concat(subdir, img)
-
             -- trying to load the image
             -- it may look inneficient to do it twice in this function but it ensures
             -- image can be loaded and that it contains 3 channels
             -- it is important to get the proper tensor size for the returned table
             -- without having to resize it
+            local img_fullpath = paths.concat(subdir, img)
             local loaded_img = image.load(img_fullpath)
+
             if loaded_img:size()[1] == 3 then
                 image_paths[ img_fullpath ] = label
             end
@@ -51,8 +49,8 @@ function load_images_and_labels( folder_path, width, height )
 
     print( 'processing images' )
     -- initialize the tensors to be returned
-    img_count = size_of(image_paths)
-    image_set = {}
+    local img_count = size_of(image_paths)
+    local image_set = {}
     image_set.label = torch.IntTensor(img_count):zero()
     image_set.data = torch.DoubleTensor(img_count, 3, width, height):zero()
 
@@ -118,15 +116,10 @@ function process_pascal_voc_dataset( path_to_images, path_to_annotations, output
             local output_dir_name = paths.concat( output_path, dir_name )
             local output_dir_class = paths.concat( output_dir_name, class_name )
 
-            if not paths.dirp( output_dir_name ) then
-                paths.mkdir( output_dir_name )
-            end
+            paths.mkdir( output_dir_name )
+            paths.mkdir( output_dir_class )
 
-            if not paths.dirp( output_dir_class ) then
-                paths.mkdir( output_dir_class )
-            end
-
-            -- read each line of the annoatation file
+            -- read each line of the annotation file
             for line in io.lines( paths.concat( path_to_annotations, txt ) ) do
 
                 -- looking for 1's only, so we expect a 2-whitespace separation
@@ -200,9 +193,7 @@ function crop_pascal_voc_dataset( path_to_xml, path_to_images, output_path )
                 local ymin = lubxml.find( bbox, 'ymin' )
 
                 local output_dir = paths.concat( output_path, class[1] )
-                if not paths.dirp( output_dir ) then
-                    paths.mkdir( output_dir )
-                end
+                paths.mkdir( output_dir )
 
                 local x1 = tonumber( xmin[1] )
                 local y1 = tonumber( ymin[1] )
@@ -212,6 +203,62 @@ function crop_pascal_voc_dataset( path_to_xml, path_to_images, output_path )
                 -- crop the image by the bounding box and save it in the folder of the proper class
                 local cropped_image = image.crop( image_to_load, x1, y1, x2, y2 )
                 image.save( paths.concat( output_dir, counter .. '.jpg' ), cropped_image )
+            end
+        end
+    end
+end
+
+-- split a dataset in 2 parts: train and test
+-- [input: path_to_images] path to folder containing the jpg images organized in labelled sub-folders
+-- [input: output_path] output folder where data will be copied to following the new folder structure (train/{label} and test/{label})
+-- [input: split_ratio] % of images to copy in the train dataset. The rest is copied to test dataset
+function split_dataset( path_to_images, output_path, split_ratio )
+    if not paths.dirp( output_path ) then
+        print( 'output directory does not exist' )
+        return
+    end
+
+    if split_ratio < 0  or split_ratio > 1 then
+        print( 'split ratio must be in range [0,1]')
+        return
+    end
+
+    local train_path = paths.concat( output_path, 'train' )
+    local test_path = paths.concat( output_path, 'test' )
+
+    paths.mkdir( train_path )
+    paths.mkdir( test_path )
+
+    -- for each sub directory
+    for label in paths.iterdirs(path_to_images) do
+        local input_subdir = paths.concat(path_to_images, label)
+        local train_sub_dir = paths.concat(train_path, label)
+        local test_sub_dir = paths.concat(test_path, label)
+
+        paths.mkdir( train_sub_dir )
+        paths.mkdir( test_sub_dir )
+
+        -- get the total count of file
+        local count = 0
+        for img in paths.iterfiles( input_subdir ) do
+            count = count + 1
+        end
+
+        local train_size = math.floor(split_ratio * count)
+        local total_count = count
+        count = 0
+
+        print( 'working on ' .. label )
+
+        -- copy files to test or train based on split ratio
+        for img in paths.iterfiles( input_subdir ) do
+            count = count + 1
+            xlua.progress( count, total_count )
+
+            if count > train_size then
+                os.execute( 'cp ' .. paths.concat( input_subdir, img ) .. ' ' .. test_sub_dir )
+            else
+                os.execute( 'cp ' .. paths.concat( input_subdir, img ) .. ' ' .. train_sub_dir )
             end
         end
     end
